8cce82fae7b55f5ef95ed85607e293f5410dc186
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-02-10 16:32:25 -0500

    delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,23 +1,9 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-    }
-    else if (!places[id]) {
-      res.render('error404')
-    }
-    else {
-      res.render('places/show', { place: places[id]})
-    }
+router.get("/", (req, res) => {
+    res.render("places/index", { places });
   })
-  
-  
-  
-
-// More code ...
 
 
 router.post('/', (req, res) => {
@@ -33,6 +19,44 @@ router.post('/', (req, res) => {
     }
     places.push(req.body)
     res.redirect('/places')
+  });
+  
+  router.get("/new", (req, res) => {
+    res.render("places/new");
+  });
+
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id], id })
+    }
   })
+  
+  
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+  
+
+// More code ...
+
+
+
   
 module.exports = router 
